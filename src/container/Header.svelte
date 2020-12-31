2dcca7bd0d8e3d0e07eda8f9708a43a0227f07c0
Sanggu Han <korean139@gmail.com>
2020-12-31 13:03:17 +0900

    implement basic header (#10)
    implement #8

---
+++ src/container/Header.svelte
@@ -0,0 +1,76 @@
+<script>
+</script>
+
+<style>
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+
+    font-family: Helvetica;
+  }
+
+  .container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    text-align: center;
+  }
+
+  .title {
+    font-size: 64px;
+    font-weight: 700;
+    margin: auto 0;
+  }
+
+  .search,
+  .settings {
+    font-size: 24px;
+    font-weight: 300;
+    color: rgba(0, 0, 0, 0.3);
+    margin: auto 0;
+  }
+
+  @media (max-width: 759px) {
+    .container {
+      display: block;
+    }
+
+    .search,
+    .settings {
+      display: none;
+    }
+  }
+
+  @media (min-width: 760px) {
+    .container {
+      width: 700px;
+      margin: 0 auto;
+    }
+  }
+
+  @media (min-width: 1020px) {
+    .container {
+      width: 1000px;
+      margin: 0 auto;
+    }
+  }
+
+  @media (min-width: 1280px) {
+    .container {
+      width: 1200px;
+      margin: 0 auto;
+    }
+  }
+</style>
+
+<div data-testid="header" class="header">
+  <div data-testid="container" class="container">
+    <!-- <slot name="search" /> -->
+    <div data-testid="search" class="search">Search</div>
+    <div data-testid="title" class="title">Panoptes</div>
+    <!-- <slot name="settings" /> -->
+    <div data-testid="settings" class="settings">Settings</div>
+  </div>
+</div>
