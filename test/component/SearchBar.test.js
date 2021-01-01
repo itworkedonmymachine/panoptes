f50b738a3f9965e740931d87b0aab1a0c0d4bc95
Seunghyun Shin <aass2356@gmail.com>
2021-01-01 19:34:34 +0900

    feat(add searchbar): add searchbar and jest
    en #6

---
+++ test/component/SearchBar.test.js
@@ -0,0 +1,66 @@
+import { render } from '@testing-library/svelte';
+import SearchBar from '../../src/component/SearchBar.svelte';
+
+describe('Render search bar', () => {
+  it('should render search bar and moddable', async () => {
+    const { getByTestId } = render(SearchBar);
+
+    const searchbar = getByTestId('searchbar');
+
+    expect(searchbar).toBeVisible();
+    expect(searchbar).toHaveAttribute('type', 'search');
+    expect(searchbar).toHaveAttribute('placeholder', 'enter platform');
+    expect(searchbar).toBeEnabled();
+  });
+
+  it('should render platform list properly', async () => {
+    const { getByTestId } = render(SearchBar);
+
+    const platformlist = getByTestId('platformList');
+
+    expect(platformlist.childElementCount).toBe(3);
+    expect(platformlist.children.item(0).children.item(0)).toHaveAttribute(
+      'value',
+      'GitHub'
+    );
+  });
+
+  it('should render platform list properly if list is given', async () => {
+    const { getByTestId } = render(SearchBar, {
+      props: {
+        platforms: [
+          { id: 'Docker', status: 'Bad' },
+          { id: 'NPM', status: 'Good' },
+        ],
+      },
+    });
+
+    const platformlist = getByTestId('platformList');
+
+    expect(platformlist.childElementCount).toBe(2);
+    expect(platformlist.children.item(0).children.item(0)).toHaveAttribute(
+      'value',
+      'Docker'
+    );
+    expect(platformlist.children.item(1).children.item(0)).toHaveAttribute(
+      'value',
+      'NPM'
+    );
+  });
+
+  it('should render platform list properly if user input is given', async () => {
+    const { getByTestId } = render(SearchBar, {
+      props: {
+        userInput: 'Doc',
+      },
+    });
+
+    const platformlist = getByTestId('platformList');
+
+    expect(platformlist.childElementCount).toBe(1);
+    expect(platformlist.children.item(0).children.item(0)).toHaveAttribute(
+      'value',
+      'Docker'
+    );
+  });
+});
