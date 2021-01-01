2d4bf8b5b54a5b91208c7800dd848f1ee4d5ae55
Sanggu <korean139@gmail.com>
2021-01-01 13:50:37 +0900

    chore(eslint): turn off rules not working w/ svelte
    ref:
    https://github.com/sveltejs/eslint-plugin-svelte3/blob/master/OTHER_PLUGINS.md#eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,14 @@ module.exports = {
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
+      rules: {
+        'import/first': 'off',
+        'import/no-mutable-exports': 'off',
+      },
     },
   ],
   rules: {
     'import/no-extraneous-dependencies': 'off',
-    'import/no-mutable-exports': 'off',
     'import/prefer-default-export': 'off',
   },
 };
