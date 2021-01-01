44513e35fd00362fe7fab2cc9876f371f05e3cc8
Sanggu <korean139@gmail.com>
2021-01-01 23:18:42 +0900

    feat(modal): implement modal component
    implements #22

---
+++ src/component/Modal.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<style>
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    background: #c4c4c4;
+    opacity: 0.9;
+
+    z-index: 139;
+  }
+
+  .container {
+    z-index: 140;
+  }
+</style>
+
+<div data-testid="overlay" class="modal-overlay">
+  <div data-testid="container" class="container">
+    <slot />
+    <!-- to get rid of received an unexpected slot "default" -->
+    <!-- ref: https://github.com/sveltejs/svelte/issues/4546 -->
+    {#if false}
+      <slot />
+    {/if}
+  </div>
+</div>
