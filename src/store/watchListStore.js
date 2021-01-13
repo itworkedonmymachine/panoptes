ad423122b7ae113c2cd9bcb21cf4d66b1a92563d
Sanggu <korean139@gmail.com>
2021-01-14 04:03:24 +0900

    fix(store): marshal & parse JSON platforms for local storage

--- src/store/watchListStore.js
+++ src/store/watchListStore.js
@@ -1,28 +1,14 @@
-import { writable, derived, get } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 // import statusStorePool from './statusStorePool';
-/*
-1. statusStorePool => parameter로 넘기지 말고 호출만 하는걸로
-2. 
-*/
+
 export const setListLocalStorage = (platforms) => {
-  // in object type
-  // localStorage.setItem('watchList', JSON.stringify(selectedPlatrorms));
-  // in array type
-  localStorage.setItem('watchList', get(platforms));
+  localStorage.setItem('watchList', JSON.stringify(platforms));
 };
 
-const getListLocalStorage = () =>
-  // in object type
-  // JSON.parse(localStorage.getItem('watchList'));
-  // in array type
-  localStorage.getItem('watchList');
-
-/*
-1. local storage에 저장된 watchlist (selected list)가 있는지 확인한다
-
-2. 유저가 선택한 플랫폼을 반영한다
-3. 유저가 선택한 플랫폼을 local storage에 반영한다
-*/
+const getListLocalStorage = () => {
+  const watchListJsonString = localStorage.getItem('watchList');
+  return JSON.parse(watchListJsonString);
+};
 
 const setSelectedPlatforms = () => {
   const localStorageList = getListLocalStorage();
