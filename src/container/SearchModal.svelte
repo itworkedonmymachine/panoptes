45e15e4ca0fc68ab344e2039fbd1e67b92e1d1f4
Sanggu <korean139@gmail.com>
2021-01-24 05:12:21 +0900

    feat(search-bar-modal): define search bar modal as container

---
+++ src/container/SearchModal.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Modal from '../component/Modal.svelte';
+  import SearchBar from '../component/SearchBar.svelte';
+  import { showSearchBar } from '../store/modalStore';
+
+  const closeSearchBar = () => {
+    $showSearchBar = false;
+  };
+</script>
+
+<style>
+</style>
+
+<Modal show={$showSearchBar} onClose={closeSearchBar}>
+  <SearchBar />
+</Modal>
